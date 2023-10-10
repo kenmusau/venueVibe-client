c6d9b899e0344b2cdb0a540dafe6ede746aa0d32
Kennedy Musau <kennedy.musauu@gmail.com>
2023-10-10 16:46:05 +0300

    add sign up form

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,10 +1,12 @@
 import "./PageNotFound.css";
+import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
   return (
     <div className="page-not-found">
       <h1>404 - Page Not Found</h1>
       <p>The page you're looking for doesn't exist.</p>
+      <Link to="/">Home</Link>
     </div>
   );
 };
--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -1,7 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import "./SignUpForm.css";
+import { baseUrl } from "../../utils";
+import Swal from "sweetalert2";
 
 function SignUpForm() {
-  return <div>SignUpForm</div>;
+  const { register, handleSubmit } = useForm();
+  const [loading, isLoading] = useState(false);
+
+  const onSubmit = (data) => {
+    isLoading(true);
+    const formData = {
+      username: data.username,
+      first_name: data.first_name,
+      last_name: data.last_name,
+      email: data.email,
+      password: data.password,
+      profile_picture: data.profile_picture,
+    };
+    axios
+      .post(`${baseUrl}/clientSignup`, formData)
+      .then((res) => {
+        isLoading(false);
+        Swal.fire({
+          title: "Success!",
+          text: "Your account was successfully created",
+          icon: "success",
+          confirmButtonText: "Okay",
+        });
+      })
+      .catch((errors) => {
+        console.log(errors.message);
+        isLoading(false);
+      });
+  };
+
+  return (
+    <div className="sign-up-container">
+      <div className="form-details">
+        <h1>Create Account</h1>
+
+        <form onSubmit={handleSubmit(onSubmit)} className="signup-form">
+          <label htmlFor="username">Username</label>
+          <input type="text" {...register("username")} />
+
+          <label htmlFor="first_name">First Name</label>
+          <input type="text" {...register("first_name")} />
+
+          <label htmlFor="last_name">Last Name</label>
+          <input type="text" {...register("last_name")} />
+
+          <label htmlFor="email">Email</label>
+          <input type="email" {...register("email")} />
+
+          <label htmlFor="profile_picture">Profile picture</label>
+          <input type="text" {...register("profile_picture")} />
+
+          <label htmlFor="password">Password</label>
+          <input type="password" {...register("password")} />
+
+          {/* <label htmlFor="confirm_password">Confirm Password</label>
+          <input type="password" {...register("confirm_password")} /> */}
+
+          <button className="signup-btn">
+            {loading ? "Loading..." : "Submit"}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default SignUpForm;
