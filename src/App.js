da4bbf8f2659a57f85fe58ae97ba874083ae4cc4
gykunju <alvingikunju@gmail.com>
2023-10-11 23:26:03 +0300

    add react-hook-form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css'
 import Login from './pages/Login';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [user, setUser] = useState({})
+
+  useEffect(()=>{
+    console.log(user)
+  },[user])
   return (
     <div className="App">
-      <h2>Venue Vibe Loading ......</h2>
+      <div className='App'>
+      {/* <h2>Venue Vibe Loading ......</h2> */}
       <Routes>
-        <Route path="/login" element={<Login/>}/>
+        <Route path="/login" element={<Login onSave={setUser}/>}/>
       </Routes>
+      </div>
     </div>
   );
 }
