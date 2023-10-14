cc50a88820686303a74d237e38ca74c276afb9f9
Kennedy Musau <kennedy.musauu@gmail.com>
2023-10-14 17:25:01 +0300

    structrue dashboard

--- src/components/ClientDashboard/DashBoard.js
+++ src/components/ClientDashboard/DashBoard.js
@@ -3,7 +3,7 @@ import React from "react";
 function DashBoard({ client }) {
   return (
     <div>
-      <h1>Welcome {client?.data?.username?} </h1>
+      <h1>Welcome {client?.data?.username} </h1>
     </div>
   );
 }
