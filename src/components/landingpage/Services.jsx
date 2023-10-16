dd3ffab5fc63875f208c1195e65d2fa4f491ba56
Mutiso <suzanmutiso@gmail.com>
2023-10-16 16:49:27 +0300

    create a css file for the services section

--- src/components/landingpage/Services.jsx
+++ src/components/landingpage/Services.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from './navbar';
+import './Services.css';
 
 function Services() {
     const [spaces, setSpaces] = useState([]);
@@ -19,16 +20,7 @@ function Services() {
             <div className="bg-gray-100 py-16">
                 <div className="container mx-auto text-center">
                     <h2
-                        className="text-4xl font-bold mb-8"
-                        style={{
-                            color: '#000',
-                            fontFamily: 'Inter',
-                            fontSize: '32px',
-                            fontStyle: 'normal',
-                            fontWeight: 700,
-                            lineHeight: 'normal',
-                        }}
-                    >
+                        className="text-4xl font-bold mb-8 service-title">
                         Our Services
                     </h2>
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
@@ -36,29 +28,10 @@ function Services() {
                             <Link key={index} to={`/spaces/${space.id}`}>
                                 <div className="bg-white p-6 rounded-md shadow-md">
                                     <h3
-                                        className="text-xl font-bold mb-4"
-                                        style={{
-                                            color: '#000',
-                                            fontFamily: 'Inter',
-                                            fontSize: '20px',
-                                            fontStyle: 'normal',
-                                            fontWeight: 700,
-                                            lineHeight: 'normal',
-                                        }}
-                                    >
+                                        className="text-xl font-bold mb-4 service-name">
                                         {space.name}
                                     </h3>
-                                    <p
-                                        className="text-gray-600"
-                                        style={{
-                                            color: 'rgba(0, 0, 0, 0.60)',
-                                            fontFamily: 'Inter',
-                                            fontSize: '16px',
-                                            fontStyle: 'normal',
-                                            fontWeight: 500,
-                                            lineHeight: 'normal',
-                                        }}
-                                    >
+                                    <p className="text-gray-600 service-description">
                                         {space.description}
                                     </p>
                                 </div>
@@ -71,27 +44,11 @@ function Services() {
                 <div className="container mx-auto text-center">
                     <div className="w-732 h-88 flex-shrink-0">
                         <p
-                            className="text-black font-bold text-4xl mb-4"
-                            style={{
-                                fontFamily: 'Inter',
-                                fontSize: '48px',
-                                fontStyle: 'normal',
-                                fontWeight: 700,
-                                lineHeight: 'normal',
-                            }}
-                        >
+                            className="text-black font-bold text-4xl mb-4">
                             Book a space that suits you.
                         </p>
                         <p
-                            className="text-black text-lg mb-8"
-                            style={{
-                                fontFamily: 'Inter',
-                                fontSize: '32px',
-                                fontStyle: 'normal',
-                                fontWeight: 500,
-                                lineHeight: 'normal',
-                            }}
-                        >
+                            className="text-black text-lg mb-8">
                             Be inspired to meet, create and innovate.
                         </p>
 
@@ -102,7 +59,7 @@ function Services() {
                                     className="w-282 h-283 flex-shrink-0"
                                     style={{
                                         borderRadius: '10px',
-                                        background: `url(path-to-image-${imageIndex}.jpg), lightgray 50% / cover no-repeat, #D9D9D9`,
+                                        background: `url(path-to-image-${imageIndex}.jpg) lightgray 50% / cover no-repeat, #D9D9D9`,
                                     }}
                                 ></div>
                             ))}
