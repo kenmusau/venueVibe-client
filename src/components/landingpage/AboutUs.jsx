ca9f2d6af9d97f998a98ac951be4668aeff08220
Mutiso <suzanmutiso@gmail.com>
2023-10-16 17:31:51 +0300

    Adding a css file for the About Us

--- src/components/landingpage/AboutUs.jsx
+++ src/components/landingpage/AboutUs.jsx
@@ -1,52 +1,27 @@
 import React from 'react';
 import Navbar from './navbar';
+import './AboutUs.css';
 
+import aboutUsImage from ''; 
 
 function AboutUs() {
   return (
     <div>
-        <Navbar />
-        <div className="bg-gray-100 py-16 flex">
-        <div className="container mx-auto text-center flex items-center justify-center">
-            <div
-             className="w-720 h-837 flex-shrink-0"
-             style={{
-               borderRadius: '10px',
-               background: `url(${aboutUsImage}), lightgray 50% / cover no-repeat, #D9D9D9`,
-             }}
-            ></div>
-            <div className="ml-12" >
-                <h2
-                className="text-4xl font-bold mb-8"
-                style={{
-                  color: '#000',
-                  fontFamily: 'Inter',
-                  fontSize: '64px',
-                  fontStyle: 'normal',
-                  fontWeight: 700,
-                  lineHeight: 'normal',
-                }}
-              >
-                About Us 
-                </h2>
-                <p
-                 className="text-lg mb-6"
-                 style={{
-                   color: 'rgba(0, 0, 0, 0.60)',
-                   fontFamily: 'Inter',
-                   fontSize: '30px',
-                   fontStyle: 'normal',
-                   fontWeight: 500,
-                   lineHeight: '40px',
-                 }}
-               >
-                 At Venue-Vibe, our mission is to bring together like-minded people to create and
-                 celebrate in the various spaces we have. We offer creative working environments to
-                 suit your needs.
-                 </p>
-             </div>
-            </div>
+      <Navbar />
+      <div className="container mx-auto text-center flex items-center justify-center">
+        <div
+          className="image-container"
+          style={{ background: `url(${aboutUsImage}) lightgray 50% / cover no-repeat, #D9D9D9` }}
+        ></div>
+        <div className="text-container">
+          <h2 className="title">About Us</h2>
+          <p className="description">
+            At Venue-Vibe, our mission is to bring together like-minded people to create and
+            celebrate in the various spaces we have. We offer creative working environments to
+            suit your needs.
+          </p>
         </div>
+      </div>
     </div>
   );
 }
