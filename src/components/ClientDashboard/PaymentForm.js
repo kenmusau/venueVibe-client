cfacfc2222196d3682799f7b702c5e501c609a90
Kennedy Musau <kennedy.musauu@gmail.com>
2023-10-19 21:43:43 +0300

    add error styling in payments

--- src/components/ClientDashboard/PaymentForm.js
+++ src/components/ClientDashboard/PaymentForm.js
@@ -1,34 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import "./PaymentForm.css";
 import { baseUrl } from "../../utils";
+import axios from "axios";
 
-function PaymentForm({ totalAmount, onClose, bookingData }) {
-  console.log(bookingData);
+function PaymentForm({
+  totalAmount,
+  onClose,
+  bookingData,
+  onUpdateSpaceStatus,
+  setNotification,
+}) {
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [errors, setErrors] = useState([]);
+  //   console.log(bookingData);
   const { id } = bookingData;
 
   const paymentDate = new Date().toISOString();
+
   const handlePaymentSubmit = () => {
     // Simulate payment processing (replace with actual payment integration)
-    fetch(`${baseUrl}/payments`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        amount: totalAmount,
-        status: true,
-        booking_id: id,
-        payment_date: paymentDate,
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
+    axios
+      .post(
+        `${baseUrl}/payments`,
+        {
+          amount: totalAmount,
+          status: true,
+          booking_id: id,
+          payment_date: paymentDate,
+          phone_number: phoneNumber,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((response) => {
+        onUpdateSpaceStatus();
+        console.log(response.data);
         onClose();
+        setNotification("Payment successfully Received!");
+        setTimeout(() => {
+          setNotification(null);
+        }, 3000);
       })
-      .catch((errors) => {
-        // setErrors(error);
-        console.error("Error creating booking:", errors.errors.errors);
+      .catch((error) => {
+        console.error("Error creating booking:");
+        setErrors(error.response.data.errors);
       });
   };
 
@@ -37,14 +55,19 @@ function PaymentForm({ totalAmount, onClose, bookingData }) {
       <h1>Payment</h1>
       <p>Total Amount: KSH {totalAmount}</p>{" "}
       {/* Replace with actual total amount */}
-      <label>Card Number:</label>
-      <input type="text" placeholder="Card Number" />
-      <label>Expiry Date:</label>
-      <input type="text" placeholder="MM/YY" />
-      <label>CVC:</label>
-      <input type="text" placeholder="CVC" />
+      <label>Phone Number:</label>
+      <input
+        type="text"
+        placeholder="254123456789"
+        onChange={(e) => setPhoneNumber(e.target.value)}
+      />
+      {errors.map((error, index) => (
+        <p key={index} className="payment-errors">
+          {error}
+        </p>
+      ))}
       <button className="payment-btn btn-submit" onClick={handlePaymentSubmit}>
-        Submit Payment
+        Pay
       </button>
     </div>
   );
